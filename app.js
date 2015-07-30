11a5868a23c807b3bdac4fbfc6f45dbd006c869b
Jennifer Jones <jybjones@gmail.com>
2015-07-30 09:47:19 -0500

    added some testing and also colors using CHALK

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+ var chalk = require('chalk');
+
  require('./lib/server')(1337);
 
- console.log('Server running on http://localhost:1337');
+ console.log(chalk.blue('testing colors blue'));
+ console.log('Server running on' + chalk.blue.bold.underline("http://localhost:1337"));
+
